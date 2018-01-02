661f00b3457efcba4ac5d8c0fa765d7a5ce5690c
thangamani9595 <35017107+thangamani9595@users.noreply.github.com>
2018-01-02 22:20:51 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,i,count=0;
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{if(a%i==0)
+{
+count=count+1;
+}
+}
+if(count==2)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
